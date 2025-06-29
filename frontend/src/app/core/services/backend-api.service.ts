13f3242f0db02471daf7fe929245aa6106f51cbf
Jose Garrido <jg@dialogs.com>
2025-06-29 12:13:07 -0400

    parse results correctly from the backend

--- frontend/src/app/core/services/backend-api.service.ts
+++ frontend/src/app/core/services/backend-api.service.ts
@@ -1,8 +1,12 @@
 import { inject, Injectable } from '@angular/core';
 import { GoverknowerAPIService } from './goverknower-api.service';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { env } from '../../../environment/environment';
+
+interface BackendResponse {
+    response: string;
+}
 
 @Injectable({
     providedIn: 'root',
@@ -11,14 +15,17 @@ export class BackendAPIService implements GoverknowerAPIService {
     private http = inject(HttpClient);
 
     public sendMessage(message: string): Observable<string> | null {
-        let response: Observable<any> | null = null;
-
         try {
-            response = this.http.get(env.API_URL + message);
+            const response = this.http.get<BackendResponse>(env.API_URL + message);
+            return response.pipe(
+                map((data) => {
+                    console.log("[Backend API]", data);
+                    return data.response;
+                })
+            );
         } catch (error) {
             console.log(error);
+            return null;
         }
-
-        return response;
     }
 }
