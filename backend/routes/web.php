955a413d6da6acb3f3940efcf945552fe6138162
Jose Garrido <jg@dialogs.com>
2025-06-29 10:42:06 -0400

    Redirect requests from / to /app

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoverknowerController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/app');
 });
 
 Route::get('/ask', [GoverknowerController::class, 'ask']);
