fb7b52730a150342f98eff44824fd1509f68dead
Trung (Daniel) An <fifty8co@gmail.com>
2025-07-02 14:45:14 -0400

    Add thinking chat bubble for AI

--- frontend/src/app/core/components/conversation-box.component.ts
+++ frontend/src/app/core/components/conversation-box.component.ts
@@ -1,26 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { ConversationController } from '../controllers/conversation.controller';
 import { Message } from '../models/message.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'conversation-box',
     templateUrl: './conversation-box.component.html',
     styleUrl: './conversation-box.component.css',
+    imports: [CommonModule],
 })
 export class ConversationBoxComponent {
     private controller = inject(ConversationController);
     public messages: Message[] = [];
+    public aiThinking = false;
 
     constructor() {
         this.controller.getConversation().msgObsever$.subscribe((newMsg) => {
             this.messages = newMsg;
         });
+
+        this.controller.aiThinking$.subscribe((thinking) => {
+            this.aiThinking = thinking;
+        })
     }
 
-    /**
-     * Handle when user clicks on an example question
-     * @param question The example question to ask
-     */
     askExample(question: string): void {
         this.controller.sendMessage(question);
     }
--- frontend/src/app/core/models/message.model.ts
+++ frontend/src/app/core/models/message.model.ts
@@ -1,6 +1,12 @@
+import * as uuid from 'uuid';
+
 export class Message {
+    public id: string;
+
     constructor(
         public text: string,
         public sender: 'user' | 'ai',
-    ) {}
+    ) {
+        this.id = uuid.v4();
+    }
 }
